3cd27b078bb3f029d9bcb177589b00b2079dcf4c
Jakob Jünger <jakob.juenger@uni-muenster.de>
2025-03-18 17:54:36 +0100

    Add comments

--- transform/dio2epi.py
+++ transform/dio2epi.py
@@ -7,7 +7,7 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 import LKSParser
 
-#%%
+#%% Extract transcriptions
 
 df = pd.read_csv("data/dio_inschriften.csv", delimiter = ';')
 
@@ -22,12 +22,14 @@ def extract_lno_text(xml_string):
 
 df['text'] = df['content'].apply(extract_lno_text)
 df = df.drop(columns=['content_clean', 'content'])
+
+# Add case numbers
 df['case'] = range(1, len(df) + 1)
 df = df.explode('text')
 df['lno'] = df.groupby('case').cumcount() + 1
 
 
-#%%
+#%% Parse all
 
 df['error'] = ""
 df['parsed'] = ""
@@ -41,13 +43,10 @@ for idx, row in df.iterrows():
         df.loc[idx, 'error'] = str(e)
 
 
-#%%
+#%% Save
 tests = ""
 for idx, row in df.iterrows():
-    #test = "C" + str(row['case']) + 'L' + str(row['lno']) + ' "' + row['text'] + '" '
-    test = "C" + str(row['case']) + 'L' + str(row['lno']) + ": " + str(row['text'])
-    tests = tests + "\n" + test
+    tests = tests + "\n" + "C" + str(row['case']) + 'L' + str(row['lno']) + ": " + str(row['text'])
 
-#%%
 with open("tests_grammar/02_test_di_passau.ini", "w", encoding="utf-8") as file:
-    file.write("[match:di-passau]\n" + tests)
+    file.write("[match:inscription]\n" + tests)
